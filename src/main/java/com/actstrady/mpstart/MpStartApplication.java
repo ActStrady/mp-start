b052e94810f3a13a07962bb127df2aa2646d0392
WangQiang <actstrady@gmail.com>
2019-11-25 15:45:09 +0800

    Initial commit

---
+++ src/main/java/com/actstrady/mpstart/MpStartApplication.java
@@ -0,0 +1,13 @@
+package com.actstrady.mpstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/actstrady/mpstart/MpStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actstrady.mpstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
